478db2c49063c72895be8300969b847ed7e052df
Olivia Auzenne <oauzenne@gmail.com>
2019-02-21 17:07:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,12 @@
 class Shoe
-  def initalize(brand)
-    @brand=brand
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  def initialize(brand)
+    @brand = brand
   end
-  
-  def brand
-    @brand="Nike"
-  end
-  
-  def cobble
-      @condition = "new"
-      puts "Your shoe is as good as new!"
-  end
-  
 
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 end
