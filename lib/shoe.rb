9c57f30584662604470cc856a6b40fc427a43e76
Olivia Auzenne <oauzenne@gmail.com>
2019-02-21 16:56:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,14 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
-  attr_reader 
-    :brand
   def initalize(brand)
     @brand=brand
+  end
+  
+  def cobble
+      puts "The shoe has been repaired"
+  end
+  
+
+end
